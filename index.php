37936ba678a5ca5bd4bd1dbe9bdf7251a4a99c87
William Oliveira de Lagos <william.lagos@live.com>
2018-02-19 01:13:51 -0300

    Added source tree

---
+++ index.php
@@ -0,0 +1,297 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./icon.svg">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <style>
+      html, body {
+        height:100%;
+        width:100%;
+        margin:0;
+        overflow-x:hidden;
+      }
+      h1,h2,h3,h4,strong {
+        color:orange;
+      }
+      .blockchain {
+        background-size: cover !important;
+        background: url('./blockchain.jpg');
+      }
+      .orange {
+        background:#FAA818 !important;
+        opacity:0.9;
+        min-height:100%;
+        height:100%;
+        display:flex;
+        align-items:center;
+        align-content:center;
+      }
+      .white {
+        background: white;
+      }
+      .cover {
+        font-size:3.5em;
+        text-transform: uppercase;
+        color:#A96003;
+        text-align:center;
+        min-width:100%;
+        width:100%;
+      }
+      .footer {
+        /* position: absolute; */
+        /* bottom: 0; */
+        /* height: 60px; */
+        /* line-height: 60px; */
+        width: 100%;
+        background-color: #FAA818;
+        color: white;
+      }
+      .overlay {
+        background:black;
+        opacity:0.8;
+        position:absolute;
+        top:0;
+        align-items:center;
+        text-align:center;
+        height:100%;
+      }
+      .column {
+        background-size:cover !important;
+        background-position:center center !important;
+        min-height:720px;
+      }
+    </style>
+
+    <title>Grandir Capital Financeiro</title>
+  </head>
+  <body class="blockchain">
+    <nav class="container-fluid orange">
+      <h1 class="cover">Grandir
+        <div style="color:white; font-size:.5em">Capital Financeiro</div>
+      </h1>
+    </nav>
+
+    <main class="container-fluid white py-5">
+      <div class="container">
+        <div class="row py-4">
+          <div class="col-md-4">
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="./business.jpg"/>
+              <div class="card-img-overlay d-flex" style="background:black; opacity:0.6; align-items:center">
+                <h2 class="text-center text-uppercase" style="width:100%">A empresa</h2>
+              </div>
+            </div>
+            <div class="my-5">
+              <p class="lead text-justify">
+                A Grandir capital financeiro é uma Holding mista, localizada em Porto Alegre, RS
+                fundada em agosto de <strong>2015</strong> por dois amigos que, após trabalharem
+                juntos em grandes empresas como <strong>GRUPO RBS, SANTANDER, SEBRAE</strong> e
+                outras, decidiram aplicar os seus conhecimentos obtidos com estas experiências
+                em um projeto próprio.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="./target.jpg"/>
+              <div class="card-img-overlay d-flex" style="background:black; opacity:0.6; align-items:center">
+                <h2 class="text-center text-uppercase" style="width:100%">Missão</h2>
+              </div>
+            </div>
+            <div class="my-5">
+              <p class="lead text-justify">
+                <strong>AGREGAR VALOR AOS NOSSOS PARCEIROS, EQUIPE E SOCIEDADE EM GERAL</strong>,
+                oferecendo a estes a oportunidade de obter melhores rendimentos sobre seu capital,
+                beneficiando-se da tecnologia <strong>BLOCKCHAIN</strong> e o mercado das criptomoedas.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="./future.jpg"/>
+              <div class="card-img-overlay d-flex" style="background:black; opacity:0.6; align-items:center">
+                <h2 class="text-center text-uppercase" style="width:100%">Visão</h2>
+              </div>
+            </div>
+            <div class="my-5">
+              <p class="lead text-justify">
+                Até <strong>AGOSTO DE 2020</strong> formar o Grupo Grandir, tornando-se sócios e
+                criando empresas de diferentes segmentos tradicionais do mercado, se consolidando
+                como uma das <strong>MAIS PROMISSORAS HOLDING MISTA</strong> do Rio Grande do Sul.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row py-5">
+        <div class="col-md-12 text-center">
+          <h3 class="text-uppercase">Valores</h3>
+          <h5 class="text-uppercase">Solidez, humildade e transparência</h5>
+          <div class="row py-5">
+            <div class="col-md-4 p-0 column" style="background:url('./solidity.jpg');"/>
+              <div class="d-flex overlay">
+                <p class="text-center p-5" style="color:white; font-size:1.4em">
+                  <strong>SOLIDEZ:</strong> Buscar oportunidades que apresentem o melhor <strong>
+                  CUSTO BENEFICIO</strong> em relação ao risco e potencial de ganho. O nosso principal
+                  objetivo é <strong>ASSEGURAR</strong> o capital de nosso parceiro oferecendo ao mesmo
+                  um retorno condizente com a realidade para mantermos a parceria por um longo prazo.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4 p-0 column" style="background:url('./respect.jpg');">
+              <div class="d-flex overlay">
+                <p class="text-center p-5" style="color:white; font-size:1.4em">
+                  <strong>HUMILDADE:</strong> No trato com o público em geral (parceiros, funcionários,
+                  fornecedores). Buscando uma relação de <strong>RESPEITO</strong> independente da classe
+                  social, cor ou credo, a postura da Grandir irá se manter sempre a mesma <strong>
+                  HUMILDADE PARA APRENDER</strong>, pois nós estamos em constante evolução!
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4 p-0 column" style="background:url('./compliance.jpg');"/>
+              <div class="d-flex overlay">
+                <p class="text-center p-5" style="color:white; font-size:1.4em">
+                  <strong>TRANSPARÊNCIA:</strong> Deixaremos os parceiros sempre informados da atual
+                  situação da Grandir, repassando informações importantes de novos negócios e sobre o
+                  crescimento da empresa.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row py-4">
+          <div class="col-md-6 d-flex" style="align-items:center">
+            <h1 class="text-center text-uppercase mb-5">O que fazemos</h1>
+          </div>
+          <div class="col-md-6 d-flex" style="align-items:center">
+            <p class="lead text-center">
+              Atuamos no mercado de <strong>CRIPTOMOEDAS</strong>, com trade, arbitragem e fundos em
+              criptomoedas. Fazemos análises do mercado <strong>BUSCANDO AS MELHORES OPORTUNIDADES DE
+              GANHO</strong>. Trabalhamos com a diversificação do capital fazendo a gestão de risco.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row" style="background:url('./chain.jpg'); background-size:cover;">
+        <div class="py-5" style="background:black; opacity:0.9; height:100%; width:100%;">
+          <div class="container" style="color:white">
+            <div class="row py-4">
+              <div class="col-md-12">
+                <div class="pb-5">
+                  <h3 class="text-uppercase text-center">Criptomoedas</h3>
+                  <h5 class="text-uppercase text-center">Você já ouviu falar de Bitcoin?</h5>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <p class="text-uppercase">
+                      <strong>2008</strong> Nasce o Bitcoin <br/>
+                      <strong>2009</strong> Início da Comercialização do Bitcoin <br/>
+                      <strong>2010</strong> 10.000 Bitcoin por uma pizza <br/>
+                      <strong>2010</strong> Valor do Bitcoin <strong>US$ 10</strong> <br/>
+                      <strong>2017</strong> Bitcoin ultrapassa os <strong>US$ 15.000</strong> <br/>
+                    </p>
+                  </div>
+                  <div class="col-md-6">
+                    <p class="text-uppercase">
+                      <strong>2017</strong> Google abre caminho para que qualquer um compre na Internet usando Bitcoin <br/>
+                      <strong>2017</strong> Bolsa de Chicago anuncia lançamento de contratos futuro de Bitcoin <br/>
+                      <strong>2017</strong> Bolsa de Tóquio anuncia lançamento de contratos futuro de Bitcoin <br/>
+                    </p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <p class="lead text-justify">
+                      Bitcoin é uma <strong>MOEDA VIRTUAL</strong> e a primeira <strong>REDE DE PAGAMENTO
+                      DESCENTRALIZADA</strong> onde os usuários é que gerenciam o sistema, <strong>SEM
+                      </strong> necessidade de <strong>INTERMEDIADOR</strong> como bancos ou autoridade
+                      central.
+                    </p>
+                  </div>
+                  <div class="col-md-6">
+                    <p class="lead text-justify">
+                      Trata-se de uma rede que <strong>FUNCIONA DE FORMA CONSENSUAL</strong>
+                      onde foi possível criar uma nova forma de pagamento e também uma
+                      <strong>NOVA MOEDA</strong> completamente digital.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+      <div class="row py-4">
+        <div class="col-md-12 text-center py-5">
+          <h3 class="text-uppercase">Seja parceiro da Grandir</h3>
+          <h5 class="text-uppercase">E lucre 2,5% ao mês</h5>
+        </div>
+        <div class="col-md-6">
+          <p class="lead text-justify">
+            <strong>COMO?</strong> Através de um contrato de empréstimo registrado em
+            cartório, firmamos o compromisso de lhe pagar <strong>2,5% AO MÊS DURANTE 12
+            MESES</strong>.
+          </p>
+        </div>
+        <div class="col-md-6">
+          <p class="lead text-justify">
+            Ao final deste período você pode <strong>RESGATAR SEU CAPITAL</strong>
+            emprestado de volta ou renovar o contrato de empréstimo por mais 12 meses.
+          </p>
+        </div>
+        <div class="col-md-12 py-5">
+          <h6 class="text-center text-uppercase">Informações adicionais:</h6>
+          <p class="text-justify">
+            Não trabalhamos com juros compostos, desta forma aceitamos apenas 1 contrato
+            de empréstimo de 12 meses por CPF ou CNPJ (Para que os juros pagos mensalmente
+            não possam ser reinvestidos).
+          </p>
+        </div>
+      </div>
+      </div>
+    </main>
+
+    <footer class="footer py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 text-center">
+            <h2 class="text-uppercase text-white">Giovane Melo</h2>
+            <h6 class="text-uppercase" style="color:#A96003;">CEO</h6>
+            <p class="py-3">
+              <i class="fas fa-phone"></i>&nbsp;(51) 98193-3437 <br/>
+              <i class="fas fa-envelope"></i>&nbsp;ceo@grandircapitalfinanceiro.com
+            </p>
+          </div>
+          <div class="col-md-6 text-center">
+            <h2 class="text-uppercase text-white">Douglas Inacio</h2>
+            <h6 class="text-uppercase" style="color:#A96003;">Diretor Comercial</h6>
+            <p class="py-3">
+              <i class="fas fa-phone"></i>&nbsp;(51) 98163-3441 <br/>
+              <i class="fas fa-envelope"></i>&nbsp;comercial@grandircapitalfinanceiro.com
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-12">
+          <a style="color:#A96003; font-size:3em" href="https://www.facebook.com/GrandirCapitalFinanceiro/"><i class="fab fa-facebook-square"></i></a>
+          <a style="color:#A96003; font-size:3em" href="https://www.instagram.com/grandircapitalfinanceiro/"><i class="fab fa-instagram"></i></a>
+          <h2 class="text-white">grandircf</h2>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
